f8ca57f727c182453fad2ab31f53f15a96589355
Rostyslav Pobidynskyi <pobedarostik@gmail.com>
2022-06-24 04:51:56 +0300

    v

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from "react";
+import Section from "./Section/Section"
+import Statistic from "./Statistic/Statistic";
+import FeedbackOptions from "./FeedbackOptions/FeedbackOptions";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+export default function App() {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const total = () => {
+    return good + neutral + bad;
+  }
+  
+ const onLeaveFeedback = (e) => {
+   const name = e.target.name;
+
+    switch (name) {
+      case "good":
+        setGood(prev => prev + 1);
+        break;
+      case "neutral":
+        setNeutral(prev => prev + 1);
+        break;
+      case "bad":
+        setBad(prev => prev + 1);
+        break;
+      default:
+        return;
+    }
+  }
+
+  const positivePercentage = () => {
+
+    const percentag = (good * 100) / total();
+    return Math.round(percentag);
+
+  }
+
+  const objKey = ["good", "neutral", "bad"]
+
+
+  return(
+      <>
+      <Section title= "Please leave feedback">
+      <FeedbackOptions options={objKey} onLeaveFeedback={onLeaveFeedback}/>
+      
+      </Section>
+        {total() === 0 ? <p>"No feedback given"</p> : 
+      <Section title="Statistics">
+          
+      <Statistic good={good} neutral={neutral} bad={bad} total={total()} positivePercentage={positivePercentage()}/>
+      </Section>}
+
+      
+      </>
+    )
 }
 
-export default App;
